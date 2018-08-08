a1a6c5d91d5fc9a6968af5d601a3850d6e960576
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-08-07 21:00:47 -0400

    added foreign key to task model

--- models/task.js
+++ models/task.js
@@ -1,6 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
     var Task = sequelize.define('Task', {
         task_name: DataTypes.STRING,
+        project_id_fk: DataTypes.INTEGER,
         category: DataTypes.STRING,
         address: DataTypes.STRING,
         description: DataTypes.TEXT,
