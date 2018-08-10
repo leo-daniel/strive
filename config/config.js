f9fd03c875708524546e6a2496e6709bc701d991
0jordanmiller@gmail.com <0jordanmiller@gmail.com>
2018-08-09 20:43:47 -0400

    env

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = function (env) {
+  var config = {
+    "development": {
+      "username": "root",
+      "password": process.env.DB_PASS,
+      "database": "calendar_db",
+      "host": "localhost",
+      "dialect": "mysql"
+    },
+    "test": {
+      "username": "root",
+      "password": process.env.DB_PASS,
+      "database": "calendar_db",
+      "host": "localhost",
+      "dialect": "mysql",
+      "logging": false
+    },
+    "production": {
+      "use_env_variable": "JAWSDB_URL",
+      "dialect": "mysql"
+    }
+  }
+  return config[env];
+}
