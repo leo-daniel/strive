23bd5024f37bfdb74100e1659445ddfdb5569847
Christopher Mueller <christopher.mueller@cpxprojects.com>
2018-08-13 12:28:33 -0400

    created review modal

--- public/js/userInput.js
+++ public/js/userInput.js
@@ -1,70 +1,77 @@
 // hide forms until user clicks icon to select new task or goal
-$("#taskInputForm").hide();
+
 $("#goalInputForm").hide();
+$("#taskInputForm").hide();
 
-function setTask() {
-    $("#newTask").on("click", function () {
-        if ($("#goalInputForm").is(":visible")) {
-            $("#goalInputForm").hide();
-            $("#taskInputForm").show();
-        } else {
-            $("#taskInputForm").show();
-        }
+var myNewTask;
 
-        $("#submitGoal").on("click", function () {
-            // 1) hide/clear task input form.
-            $("#taskInputForm").hide();
+//Create a New Task
+$("#newTask").on("click", function(event) {
+  event.preventDefault();
+  $("#goalInputForm").hide();
+  $("#taskInputForm").show();
 
-            // 2) insert task into the calendar.
+  $("#submitMyTask").on("click", function(event) {
+    event.preventDefault();
+    // 1) Collect values from form input
+    //JSON variables to store locally until submitted
+    var myNewTask = {
+      taskTitle: (taskTitle = $("#inputTitle").val().trim()),
+      taskAddress: (taskAddress = $("#inputAddress").val().trim()),
+      taskCategory: (taskCategory = $("#inputCategory").val().trim()),
+      taskDate: (taskDate = $("#inputTaskDate").val().trim()),
+      taskPriority: (taskPriority = $("#inputPriority").val()),
+      taskHours: (taskHours = $("#inputTaskLength").val()),
+      taskNotes: (taskNotes = $("#inputNotes").val())
+    };
 
-            // 3) display "success" modal.
+    // 2) display modal with information to confirm submission of task
+    $(".modal-title").text("Confrim New Task");
+    $("#modalTitle").html("Title: "+myNewTask.taskTitle);
+    $("#modalAddress").html("Address: "+myNewTask.taskAddress);
+    $("#modalCategory").html("Category: "+myNewTask.taskCategory);
+    $("#modalDate").html("Date: "+myNewTask.taskDate);
+    $("#modalPriority").html("Priority: "+myNewTask.taskPriority);
+    $("#modalHours").html("Hours: "+myNewTask.taskHours);
+    $("#modalNotes").html("Notes: "+myNewTask.taskNotes)
+  });
 
-        });
-    });
-};
+    // 3) send data back to MySQL DB
+    
 
-function setGoal() {
+    // 4) reset form and hide
+    $("#taskInputForm")[0].reset();
+    $("#taskInputForm").hide();
+});
 
-    $("#newGoal").on("click", function () {
-        // if the task form is visible, close it and show the goal form.
-        if ($("#taskInputForm").is(":visible")) {
-            $("#taskInputForm").hide();
-            $("#goalInputForm").show();
-        } else {
-            $("#goalInputForm").show();
-        }
+//Create a New Goal
+$("#newGoal").on("click", function() {
+  // if the task form is visible, close it and show the goal form.
+  $("#taskInputForm").hide();
+  $("#goalInputForm").show();
 
-        $("#goalInputForm").show();
+  var goal = $("#description")
+    .val()
+    .trim();
 
-        var goal = $("#description").val().trim();
+  $("#submitGoal").on("click", function() {
+    // 1) hide/clear goal input form.
+    $("#goalInputForm").hide();
 
-        $("#submitGoal").on("click", function () {
-            // 1) hide/clear goal input form.
-            $("#goalInputForm").hide();
+    // 2) insert goal into the calendar.
 
-            // 2) insert goal into the calendar.
-
-            // 3) display "success" modal.
-
-        });
-    });
-};
+    // 3) display "success" modal.
+  });
+});
 
 //this function clears the input form and then hides the form
-function clearTask() {
-    $("#cancelTask").on("click", function () {
-        $("#taskInputForm").hide();
-    })
-}
+$("#cancelTask").on("click", function() {
+    //resets the form
+  $("#taskInputForm")[0].reset();
+  $("#taskInputForm").hide();
+});
+
 //clears the goal form and then hides the form
-function clearGoal() {
-    $("#cancelGoal").on("click", function () {
-        $("#goalInputForm").hide();
-    })
-}
-
-
-setTask();
-setGoal();
-clearTask();
-clearGoal();
+$("#cancelGoal").on("click", function() {
+  $("#goalInputForm").hide();
+});
