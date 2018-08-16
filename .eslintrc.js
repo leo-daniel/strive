44c5080cdcb63073723de8c120b4c3ba06ffab49
patel-km <36935963+patel-km@users.noreply.github.com>
2018-08-16 19:31:34 -0400

    A few changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
-// module.exports = {
-//     "extends": "standard"
-// };
+{
+  plugins: [prettier],
+  rules: {
+    prettier/prettier: error
+  },
+  "extends": ["plugin:prettier/recommended"]
+}
