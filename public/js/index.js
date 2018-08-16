871cea1941ae22c7cbc4b81cd3c44e652f980965
patel-km <karipatel95@hotmail.com>
2018-08-16 19:25:15 -0400

    goals update with our dbgit add views/index.handlebars

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-import bulmaCalendar from "/node_modules/bulma-extensions/bulma-calendar/dist/bulma-calendar.min.js";
+// import bulmaCalendar from "/node_modules/bulma-extensions/bulma-calendar/dist/bulma-calendar.min.js";
 
 // Get references to page elements
 var $exampleText = $("#example-text");
@@ -100,7 +100,7 @@ var handleDeleteBtnClick = function() {
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
 
-// Quote API ** NEED TO ATTRIBUTE somewhere on page.
+// Quote API ** NEED TO ATTRIBUTE somewhere on page. -------------
 // Commented out due to limit 10 API calls/hr
 
 // var queryURL = "http://quotes.rest/qod.json";
@@ -114,26 +114,87 @@ $exampleList.on("click", ".delete", handleDeleteBtnClick);
 //   $("#quote").append(quote);
 // });
 
-// Progress Chart
+// var API = {
+//   updateProgress: function(data) {
+//     return $.ajax({
+//       type: "GET",
+//       url: "/api/projects",
+//       data: JSON.stringify(data)
+//     });
+//   }
+// };
 
-// to put in Goal ajax call if the project call works.
-// var ctx4 = document.getElementById("goalTestChart").getContext("2d");
-// var goalTestChart = makeGoalChart(ctx4, [], []);
+// GOAL PROGRESS --------------------------------------------------
+$.get("/api/goals", function(data) {
+  console.log("Goals", data);
+  var goal1 = data[0].goal_name;
+  var goalProgress1 = data[0].progress;
+  var goal2 = data[1].goal_name;
+  var goalProgress2 = data[1].progress;
+  var goal3 = data[2].goal_name;
+  var goalProgress3 = data[2].progress;
+  var goalLabels = [goal1, goal2, goal3];
+  var goalProgress = [goalProgress1, goalProgress2, goalProgress3];
 
-var API = {
-  updateProgress: function(data) {
-    return $.ajax({
-      type: "POST",
-      url: "api/projects",
-      data: JSON.stringify(data)
+  console.log("Goal Labels", goalLabels);
+  console.log("Goal Data", goalProgress);
+
+  var ctx = document.getElementById("goalTestChart").getContext("2d");
+
+  var goalTestChart = makeGoalChart(ctx, goalLabels, goalProgress);
+
+  $("#goal-progress").append(goalTestChart);
+
+  function makeGoalChart(ctx, goalLabels, goalProgress) {
+    return new Chart(ctx, {
+      type: "polarArea",
+      data: {
+        labels: goalLabels,
+        datasets: [
+          {
+            label: "Points",
+            backgroundColor: ["#F5CBA7", "#F0B27A", "#CA6F1E"],
+            data: goalProgress
+          }
+        ]
+      },
+      options: {
+        animation: {
+          animateScale: true
+        }
+      }
     });
   }
-};
+});
 
-$.get("/api/projects", handleData);
+// PROJECT PROGRESS --------------------------------------------
 
-function handleData(data) {
-  console.log(data);
+function makeGoalChart(ctx, labelNames, data) {
+  return new Chart(ctx, {
+    type: "polarArea",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: "Points",
+          backgroundColor: ["#F5CBA7", "#F0B27A", "#CA6F1E"],
+          data: []
+        }
+      ]
+    },
+    options: {
+      animation: {
+        animateScale: true
+      }
+    }
+  });
+}
+
+// AJAX FOR PROJECT PROGRESS ---------------------------------------
+$.get("/api/projects", handleProjectData);
+
+function handleProjectData(data) {
+  console.log("Projects", data);
   var projectName1 = data[0].project_name;
   var projectName2 = data[1].project_name;
   var projectName3 = data[2].project_name;
@@ -196,29 +257,6 @@ function makeProjectChart(ctx, labelNames, data) {
   });
 }
 
-function makeGoalChart(ctx, labelNames, data) {
-  return new Chart(ctx, {
-    type: "polarArea",
-    data: {
-      labels: [Goal.goal_name],
-      datasets: [
-        {
-          label: "Points",
-          backgroundColor: ["#ecf0f1", "#bdc3c7", "#909497", "#626567"],
-          data: [Goal.progress]
-        }
-      ]
-    },
-    options: {
-      animation: {
-        animateScale: true
-      }
-    }
-  });
-}
-
-// $("#goal-progress").append(goalTestChart);
-
 // options for Bulma Calendar Extension
 document.addEventListener("DOMContentLoaded", function() {
   var datePickers = bulmaCalendar.attach('[type="date"]', {
@@ -229,33 +267,33 @@ document.addEventListener("DOMContentLoaded", function() {
   // datePickers now contains an Array of all datePicker instances
 });
 
-var defaultOptions = {
-  startDate: new Date(),
-  weekStart: null,
-  minDate: null,
-  maxDate: null,
-  disabledDates: null,
-  dateFormat: 'yyyy-mm-dd', // the default data format 'field' value
-  lang: 'en', // internationalization
-  overlay: false,
-  closeOnOverlayClick: true,
-  closeOnSelect: true,
-  toggleOnInputClick: true,
-  icons: {
-    month: {
-      previous: '<svg viewBox="0 0 50 80" xml:space="preserve">
-        <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
-      </svg>',
-      next: '<svg viewBox="0 0 50 80" xml:space="preserve">
-        <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
-      </svg>'
-    },
-    year: {
-      previous: '<svg viewBox="0 0 50 80" xml:space="preserve">
-        <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
-      </svg>',
-      next: '<svg viewBox="0 0 50 80" xml:space="preserve">
-        <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
-      </svg>'
-    }
-  };
+// var defaultOptions = {
+//   startDate: new Date(),
+//   weekStart: null,
+//   minDate: null,
+//   maxDate: null,
+//   disabledDates: null,
+//   dateFormat: 'yyyy-mm-dd', // the default data format 'field' value
+//   lang: 'en', // internationalization
+//   overlay: false,
+//   closeOnOverlayClick: true,
+//   closeOnSelect: true,
+//   toggleOnInputClick: true,
+//   icons: {
+//     month: {
+//       previous: '<svg viewBox="0 0 50 80" xml:space="preserve">
+//         <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
+//       </svg>',
+//       next: '<svg viewBox="0 0 50 80" xml:space="preserve">
+//         <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
+//       </svg>'
+//     },
+//     year: {
+//       previous: '<svg viewBox="0 0 50 80" xml:space="preserve">
+//         <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
+//       </svg>',
+//       next: '<svg viewBox="0 0 50 80" xml:space="preserve">
+//         <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
+//       </svg>'
+//     }
+//   };
