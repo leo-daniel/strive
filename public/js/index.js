4eeec3b09e06a6d8fbfe4a46c38cc435070a3e7d
patel-km <36935963+patel-km@users.noreply.github.com>
2018-08-16 19:31:34 -0400

    uncommented bulma

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-// import bulmaCalendar from "/node_modules/bulma-extensions/bulma-calendar/dist/bulma-calendar.min.js";
+import bulmaCalendar from "/node_modules/bulma-extensions/bulma-calendar/dist/bulma-calendar.min.js";
 
 // Get references to page elements
 var $exampleText = $("#example-text");
@@ -267,33 +267,33 @@ document.addEventListener("DOMContentLoaded", function() {
   // datePickers now contains an Array of all datePicker instances
 });
 
-// var defaultOptions = {
-//   startDate: new Date(),
-//   weekStart: null,
-//   minDate: null,
-//   maxDate: null,
-//   disabledDates: null,
-//   dateFormat: 'yyyy-mm-dd', // the default data format 'field' value
-//   lang: 'en', // internationalization
-//   overlay: false,
-//   closeOnOverlayClick: true,
-//   closeOnSelect: true,
-//   toggleOnInputClick: true,
-//   icons: {
-//     month: {
-//       previous: '<svg viewBox="0 0 50 80" xml:space="preserve">
-//         <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
-//       </svg>',
-//       next: '<svg viewBox="0 0 50 80" xml:space="preserve">
-//         <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
-//       </svg>'
-//     },
-//     year: {
-//       previous: '<svg viewBox="0 0 50 80" xml:space="preserve">
-//         <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
-//       </svg>',
-//       next: '<svg viewBox="0 0 50 80" xml:space="preserve">
-//         <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
-//       </svg>'
-//     }
-//   };
+var defaultOptions = {
+  startDate: new Date(),
+  weekStart: null,
+  minDate: null,
+  maxDate: null,
+  disabledDates: null,
+  dateFormat: 'yyyy-mm-dd', // the default data format 'field' value
+  lang: 'en', // internationalization
+  overlay: false,
+  closeOnOverlayClick: true,
+  closeOnSelect: true,
+  toggleOnInputClick: true,
+  icons: {
+    month: {
+      previous: '<svg viewBox="0 0 50 80" xml:space="preserve">
+        <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
+      </svg>',
+      next: '<svg viewBox="0 0 50 80" xml:space="preserve">
+        <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
+      </svg>'
+    },
+    year: {
+      previous: '<svg viewBox="0 0 50 80" xml:space="preserve">
+        <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375 "/>
+      </svg>',
+      next: '<svg viewBox="0 0 50 80" xml:space="preserve">
+        <polyline fill="none" stroke-width=".5em" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8 "/>
+      </svg>'
+    }
+  };
