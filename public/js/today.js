6e10a7c1e525a63b1c9c53b2d4960f27f6ccee8d
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-08-16 23:02:47 -0400

    added visual confirmation when item is checked as complete

--- public/js/today.js
+++ public/js/today.js
@@ -1,62 +1,13 @@
-$(document).ready(function () {
-    // declare variables
-    var todayContainer = $('.today-container');
-
-    // on-click event handlers
-    // TODO: add on click handlers here
-
-    // function declarations
-    function getToday() {
-        var today = Date.now();
+$('.due-item').click(function() {
+    var thisID = $(this).attr('data-id');
+    // determine whether this checkbox has been checked
+    if ($(this).prop('checked')) {
+        // TODO: tell database this is complete
+        $('#list-item-' + thisID).addClass('list-group-item-success');
+        console.log('checked');
+    } else {
+        // TODO: tell database this is not complete
+        console.log('unchecked');
+        $('#list-item-' + thisID).removeClass('list-group-item-success');
     }
 });
-
-// Getting the initial list of posts
-getPosts();
-// InitializeRows handles appending all of our constructed post HTML inside
-// blogContainer
-function initializeRows() {
-    blogContainer.empty();
-    var postsToAdd = [];
-    for (var i = 0; i < posts.length; i++) {
-        postsToAdd.push(createNewRow(posts[i]));
-    }
-    blogContainer.append(postsToAdd);
-}
-
-// build out a due today item
-function createNewRow(post) {
-    var newPostCard = $("<div>");
-    newPostCard.addClass("card");
-    var newPostCardHeading = $("<div>");
-    newPostCardHeading.addClass("card-header");
-    var deleteBtn = $("<button>");
-    deleteBtn.text("x");
-    deleteBtn.addClass("delete btn btn-danger");
-    var editBtn = $("<button>");
-    editBtn.text("EDIT");
-    editBtn.addClass("edit btn btn-default");
-    var newPostTitle = $("<h2>");
-    var newPostDate = $("<small>");
-    var newPostCategory = $("<h5>");
-    newPostCategory.text(post.category);
-
-    var newPostCardBody = $("<div>");
-    newPostCardBody.addClass("card-body");
-    var newPostBody = $("<p>");
-    newPostTitle.text(post.title + " ");
-    newPostBody.text(post.body);
-    var formattedDate = new Date(post.createdAt);
-    formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-    newPostDate.text(formattedDate);
-    newPostTitle.append(newPostDate);
-    newPostCardHeading.append(deleteBtn);
-    newPostCardHeading.append(editBtn);
-    newPostCardHeading.append(newPostTitle);
-    newPostCardHeading.append(newPostCategory);
-    newPostCardBody.append(newPostBody);
-    newPostCard.append(newPostCardHeading);
-    newPostCard.append(newPostCardBody);
-    newPostCard.data("post", post);
-    return newPostCard;
-}
