966cb1fd503fb6f7033f8a9935d6b7d6cf7e439c
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-08-17 02:26:21 -0400

    added ajax function

--- public/js/today.js
+++ public/js/today.js
@@ -1,13 +1,24 @@
 $('.due-item').click(function() {
     var thisID = $(this).attr('data-id');
+
     // determine whether this checkbox has been checked
     if ($(this).prop('checked')) {
         // TODO: tell database this is complete
         $('#list-item-' + thisID).addClass('list-group-item-success');
-        console.log('checked');
     } else {
         // TODO: tell database this is not complete
-        console.log('unchecked');
         $('#list-item-' + thisID).removeClass('list-group-item-success');
     }
 });
+
+function putAjax(data, id) {
+    $.ajax({
+      method: 'PUT',
+      url: '/api/tasks/' + id,
+      data: data
+    }).then(function(result) {});
+  }
+
+  var queryString = 'UPDATE tasks WHERE id = ? SET is_complete = ?'
+
+  // TODO: when the page loads, set checkboxes to checked if the item is complete
