0b12ce3252439d7bc809b548f142db026a0006db
Leo Daniel <danielj1122@gmail.com>
2018-08-18 00:06:17 -0400

    linting updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,3 @@
 module.exports = {
     "extends": "airbnb-base"
 };
-
-module.exports = {
-  // config source: https://github.com/neciu/eslint-config-last
-
-  "env": {
-    "node": 1,
-    "browser": 1
-  },
-  extends: ['eslint:recommended', 'prettier'], // extending recommended config and config derived from eslint-config-prettier
-  plugins: ['prettier'], // activating esling-plugin-prettier (--fix stuff)
-  rules: {
-    'prettier/prettier': [ // customizing prettier rules (unfortunately not many of them are customizable)
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'all',
-      },
-    ],
-    eqeqeq: ['error', 'always'], // adding some custom ESLint rules
-  },
-};
-
-{
-  "env": {
-    "node": 1,
-    "browser": 1
-  },
-  "globals": {
-    "exampleGlobalVariable": true
-  },
-  "rules": {
-    "eqeqeq": 1
-  },
-  "plugins": []
-}
