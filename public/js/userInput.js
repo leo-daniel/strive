5e5486d5c42bd65f70d7b43430272198c72999b4
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-08-17 23:39:57 -0400

    reverted file to master

--- public/js/userInput.js
+++ public/js/userInput.js
@@ -2,13 +2,14 @@
 var total = 0;
 $("#goalInputForm").hide();
 $("#taskInputForm").hide();
+$("#checkDateModal").hide();
 
 var myNewTask;
 var checkedProject;
 var myNewProject;
 
 //Create a New Task
-$("#newTask").on("click", function (event) {
+$("#newTask").on("click", function(event) {
   event.preventDefault();
   $("#taskInputForm").show();
   $("#showProjects").hide();
@@ -16,8 +17,47 @@ $("#newTask").on("click", function (event) {
 
   myCheckBox();
 
-  $("#submitMyTask").on("click", function (event) {
+  //onchange and check if date is taken
+  $("#checkDateButton").on("click", function(event) {
     event.preventDefault();
+
+    $("#loading").show();
+
+    $(".modal-body").hide();
+    $("#checkDateHeader").html("Superman is off...");
+
+    setTimeout(function() {
+      $("#loading").hide();
+
+      $(".modal-body").show();
+      $("#checkDateHeader").html("We've checked your date...");
+
+      var myDate = $("#inputTaskDate").val();
+      var n = moment(myDate).date();
+
+      console.log(n);
+
+      dateChecker(n).then(function(result) {
+        console.log(result);
+        if (result === "Yes") {
+          $("#checkingDate").html("Date Verified: " + myDate);
+          $("#dateChecked").html("Your all good!");
+        } else if (result === "No") {
+          $("#checkingDate").html("Date Verified: " + myDate);
+          $("#dateChecked").html("Sorry looks like your busy :(");
+        }
+      });
+    }, 2000);
+
+    // $("#checkDateModal").show()
+  });
+
+  $("#submitMyTask").on("click", function(event) {
+    event.preventDefault();
+
+    var myDate = $("#inputTaskDate").val();
+    var n = moment(myDate).date();
+
     // 1) Collect values from form input
     //JSON variables to store locally until submitted
     var myNewTask = {
@@ -37,7 +77,12 @@ $("#newTask").on("click", function (event) {
       priority: (taskPriority = $("#inputPriority").val()),
       hours_complete: (taskHours = $("#inputTaskLength").val()),
       description: (taskNotes = $("#inputNotes").val()),
-      projectId: (taskProjectID = $('#inputProjects option:selected').val())
+      project: (taskProject = $("#inputProjects")
+        .val()
+        .trim()),
+      projectId: $("#inputProjects option:selected").val(),
+      is_complete: false,
+      dateDay: n
     };
 
     // 2) display modal with information to confirm submission of task
@@ -51,15 +96,18 @@ $("#newTask").on("click", function (event) {
     $("#modalNotes").html("Notes: " + myNewTask.description);
     $("#modalProject").html("Project: " + myNewTask.projectId);
 
-    $("#confirm").on("click", function () {
+    $("#confirm").on("click", function() {
       // 3) send data back to MySQL DB
       postAjax(myNewTask, "tasks");
 
-      // let projectUpdate = {
-      //   total_tasks: 1
-      // }
+      var projectUpdate = {
+        total_tasks: 1
+      };
 
-      // putAjax(projectUpdate, 'project', $('#inputProjects option:selected').val())
+      var projectId = $("#inputProjects option:selected").val();
+      console.log("This is the project id:", projectId);
+
+      putAjax(projectUpdate, "projects", projectId);
 
       // 4) reset form and hide
       $("#taskInputForm")[0].reset();
@@ -69,7 +117,7 @@ $("#newTask").on("click", function (event) {
 });
 
 function myCheckBox() {
-  $("#inputProject").on("click", function () {
+  $("#inputProject").on("click", function() {
     if ($(this).is(":checked")) {
       checkedProject = $("#inputProject[type=checkbox]").prop("checked");
       $("#showProjects").show();
@@ -78,12 +126,12 @@ function myCheckBox() {
 }
 
 //Create a New Goal
-$("#newGoal").on("click", function () {
+$("#newGoal").on("click", function() {
   // if the task form is visible, close it and show the goal form.
   $("#taskInputForm").hide();
   $("#goalInputForm").show();
 
-  $("#submitGoal").on("click", function () {
+  $("#submitGoal").on("click", function() {
     // 1) hide/clear goal input form.
     $("#goalInputForm").hide();
 
@@ -98,31 +146,29 @@ $("#newGoal").on("click", function () {
       complete: "false"
     };
 
-
     // 3) send goals to DB
     postAjax(goalData, "goals");
 
     // 4) reset the goal form
     $("#goalInputForm")[0].reset();
     $("#goalInputForm").hide();
-
   });
 });
 
 //this function clears the input form and then hides the form
-$("#cancelTask").on("click", function () {
+$("#cancelTask").on("click", function() {
   //resets the form
   $("#taskInputForm")[0].reset();
   $("#taskInputForm").hide();
 });
 
 //clears the goal form and then hides the form
-$("#cancelGoal").on("click", function () {
+$("#cancelGoal").on("click", function() {
   $("#goalInputForm").hide();
 });
 
 //add a new project to the projects table
-$("#addProject").on("click", function () {
+$("#addProject").on("click", function() {
   // 1) collect project information
   myNewProject = {
     project_name: $("#inputProjectName")
@@ -139,7 +185,6 @@ $("#addProject").on("click", function () {
 
   // 3) reset form
   $("#projectForm")[0].reset();
-
 });
 
 // postAJAX function to put data in calendar_db
@@ -148,15 +193,17 @@ function postAjax(data, URL) {
     method: "POST",
     url: "/api/" + URL,
     data: data
-  }).then(function (result) { });
+  }).then(function(result) {});
 }
 
 function putAjax(data, URL, id) {
   $.ajax({
     method: "PUT",
-    url: "/api/" + URL + id,
+    url: "/api/" + URL,
     data: data
-  }).then(function (result) { });
+  }).then(function(result) {
+    console.log(result);
+  });
 }
 
 function postAjax2(data, URL) {
@@ -164,17 +211,65 @@ function postAjax2(data, URL) {
     method: "POST",
     url: "/api/" + URL,
     data: data
-  }).then(function (data) {
-
+  }).then(function(data) {
     $("#inputProjects").empty();
-    $.each(data, function (i, item) {
+    $.each(data, function(i, item) {
       $("#inputProjects").append(
         $("<option>", {
           value: item.id,
           text: item.project_name
         })
       );
-      console.log(item)
+      console.log(item);
     });
   });
 }
+
+function dateChecker(n) {
+  return $.ajax({
+    method: "GET",
+    url: "/checkdate/" + n
+  });
+}
+
+function getExistingProjects() {
+  $.ajax({
+    method: "GET",
+    url: "/api/projects"
+  }).then(function(data) {
+    $("#inputProjects").empty();
+
+    $.each(data, function(i, item) {
+      $("#inputProjects").append(
+        $("<option>", {
+          value: item.id,
+          text: item.project_name
+        })
+      );
+    });
+  });
+}
+
+getExistingProjects();
+
+// function getProjectsProgress(projectNumber) {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/tasks/"
+//   }).then(function (data) {
+//     var taskComplete = 0;
+//     var taskIncomplete = 0;
+//     for (i = 0; i < data.length; i++) {
+//       if (data[i].is_complete) {
+//         taskComplete++;
+//       } else if (data[i].is_complete === false) {
+//         taskIncomplete++;
+//       }
+//     }
+//     var percentage = taskComplete / taskIncomplete;
+//     console.log(taskIncomplete, taskComplete);
+//     console.log(percentage);
+//   });
+// }
+
+// getProjectsProgress();
